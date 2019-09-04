f2e8651f19e4a4bf40f66250c02f0126621218e1
Noble Obioma <nobioma1@gmail.com>
2019-09-04 17:00:45 +0100

    Convert the App component to JSX

--- src/App.js
+++ src/App.js
@@ -3,24 +3,13 @@ import { render } from 'react-dom';
 import Car from './Car';
 
 const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    // React.createElement(htmlTag, {<attributes>}, children)
-    [
-      React.createElement('h1', {}, 'Sample React'),
-      React.createElement(Car, {
-        make: 'Mercedes-Benz',
-        model: 'G-wagon',
-        year: '2019'
-      }),
-      React.createElement(Car, {
-        make: 'Lexus',
-        model: 'LC 500',
-        year: '2019'
-      })
-    ]
+  return (
+    <div>
+      <h1>Sample React</h1>
+      <Car make="Mercedes-Benz" model="G-wagon" year="2019" />
+      <Car make="Lexus" model="LC 500" year="2019" />
+    </div>
   );
 };
 
-render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
