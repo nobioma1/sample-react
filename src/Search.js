42029af82de419656e42018ecd001f94870c262c
Noble Obioma <nobioma1@gmail.com>
2019-09-04 17:30:52 +0100

    Add state to search component and onChange handler on input

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
-  const location = 'San Francisco';
+  const [location, setLocation] = useState('San Francisco');
 
   return (
     <div>
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
