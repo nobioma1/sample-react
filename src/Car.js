2e7da985643cec82ac4ee48d7672e3d2a18bad32
Noble Obioma <nobioma1@gmail.com>
2019-09-04 07:20:37 +0100

    Convert Car component to JSX

--- src/Car.js
+++ src/Car.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function Car({ make, model, year }) {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, make),
-    React.createElement('h2', {}, model),
-    React.createElement('h2', {}, year)
-  ]);
+  return (
+    <div>
+      <h1>{make}</h1>
+      <h2>{model}</h2>
+      <h2>{year}</h2>
+    </div>
+  );
 }
