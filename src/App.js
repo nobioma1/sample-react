98727de2f8b1b15ddf9a28a6a4049615923ae23a
Noble Obioma <nobioma1@gmail.com>
2019-09-08 14:59:34 +0100

    Create and use context

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import Car from './Car';
 import Search from './Search';
+import ThemeContext from './ThemeContext';
 
 const App = () => {
+  const themeHook = useState('darkblue');
   return (
-    <div>
-      <h1>Sample React</h1>
-      <Search />
-      <Car make="Mercedes-Benz" model="G-wagon" year="2019" />
-      <Car make="Lexus" model="LC 500" year="2019" />
-    </div>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <h1>Sample React</h1>
+        <Search />
+        <Car make="Mercedes-Benz" model="G-wagon" year="2019" />
+        <Car make="Lexus" model="LC 500" year="2019" />
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import ThemeContext from './ThemeContext';
 
 const Search = () => {
   const [location, setLocation] = useState('San Francisco');
-
+  const [theme] = useContext(ThemeContext);
   return (
     <div>
       <form>
@@ -15,7 +16,7 @@ const Search = () => {
             onChange={e => setLocation(e.target.value)}
           />
         </label>
-        <button>Submit</button>
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
     </div>
   );
