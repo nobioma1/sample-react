a4d653eabf06091fc3b8ac3ac070862cb581eb65
Noble Obioma <nobioma1@gmail.com>
2019-09-08 16:00:25 +0100

    Implementing @Reach router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
-import Car from './Car';
+import { Router, Link } from '@reach/router';
 import Search from './Search';
 import ThemeContext from './ThemeContext';
+import Cars from './Cars';
 
 const App = () => {
   const themeHook = useState('darkblue');
@@ -10,9 +11,12 @@ const App = () => {
     <ThemeContext.Provider value={themeHook}>
       <div>
         <h1>Sample React</h1>
-        <Search />
-        <Car make="Mercedes-Benz" model="G-wagon" year="2019" />
-        <Car make="Lexus" model="LC 500" year="2019" />
+        <Link to="/cars">Cars</Link> <br />
+        <Link to="/search">Search</Link>
+        <Router>
+          <Cars path="/cars" />
+          <Search path="/search" />
+        </Router>
       </div>
     </ThemeContext.Provider>
   );
---
+++ src/Cars.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Car from './Car';
+
+const Cars = () => {
+  return (
+    <div>
+      <Car make="Mercedes-Benz" model="G-wagon" year="2019" />
+      <Car make="Lexus" model="LC 500" year="2019" />
+    </div>
+  );
+};
+
+export default Cars;
